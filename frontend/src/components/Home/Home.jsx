548e145db095efe902a9a1236619689d2ef77bdb
PRASAD KARMALKAR <prasadkarmalkar2020@gmail.com>
2024-08-06 13:01:33 +0530

    Currency convert functionality

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,28 +1,34 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { FaRupeeSign } from "react-icons/fa";
-import { BsCurrencyDollar } from "react-icons/bs";
+import { BsCurrencyDollar, BsCurrencyRupee } from "react-icons/bs";
 import { GoArrowUpRight, GoArrowDownLeft } from "react-icons/go";
-import { IoIosSend } from "react-icons/io";
 import { UserContext } from '../../utils/userContext';
-import { createBrowserRouter, useNavigate } from 'react-router-dom';
 import PayModal from './PayModal';
 
 function Home() {
-		const {balance } = useContext(UserContext);
+		const {balance} = useContext(UserContext);
+		const [ currency, setCurrency ] = useState( 'INR' );
+		const currencyIcon = currency === 'INR' ? <BsCurrencyRupee /> : <BsCurrencyDollar />; 
+		const convertValue = useMemo(() => {
+			if ( currency === 'USD' ) {
+				return (balance/84).toFixed(2);
+			}
+			return balance;
+		}, [balance, currency]);
 	return (
 		<div className="flex-1 flex gap-4 items-center md:px-4 justify-between dark:bg-gray-700 dark:text-gray-100 ml-56 min-h-screen">
 			<div className=" w-72 p-5 rounded-3xl shadow-md bg-white dark:bg-gray-900">
 				<div className="flex">
 					<h4 className="flex-1 text-xl font-semibold">Balance</h4>
-					<button className="border p-2 rounded-md mr-2">
-						<FaRupeeSign />
+					<button onClick={()=>setCurrency('INR')} className={`border p-2 rounded-md mr-2 hover:bg-gray-900 hover:text-white dark:hover:bg-gray-100 dark:hover:text-gray-900 ${currency === 'INR' && 'bg-gray-900 text-white dark:bg-gray-100 dark:text-gray-900'}`}>
+						<BsCurrencyRupee />
 					</button>
-					<button className="border p-2 rounded-md">
+					<button onClick={()=>setCurrency('USD')} className={`border p-2 rounded-md hover:bg-gray-900 hover:text-white dark:hover:bg-gray-100 dark:hover:text-gray-900 ${currency === 'USD' && 'bg-gray-900 text-white dark:bg-gray-100 dark:text-gray-900'}`}>
 						<BsCurrencyDollar />
 					</button>
 				</div>
 				<div className="mt-3">
-					<h1 className="text-4xl font-semibold">$ {balance}</h1>
+					<h1 className="text-4xl font-semibold flex items-center">{currencyIcon} {convertValue}</h1>
 				</div>
 				<div className="mt-5">
 					<div className="flex gap-4 font-semibold">
@@ -40,6 +46,6 @@ function Home() {
 			<PayModal balance={balance} />
 		</div>
 	);
-}
+} 
 
 export default Home;
